fc4e21eeec5e02bb8d2b870173cf1e9f9b612f65
Subin Lee <ewaterbinn@gmail.com>
2025-04-02 17:39:28 +0900

    feat: 다크 모드 지원 설정

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import ThemeProvider from './lib/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider defaultTheme="system" storageKey="theme">
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
