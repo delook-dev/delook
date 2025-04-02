5a98891bb3c81ac31fd769f02d6afa4c23af436a
Subin Lee <ewaterbinn@gmail.com>
2025-04-02 15:10:47 +0900

    feat: react-router-dom 및 vite-plugin-pages를 사용하여 라우팅 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import Pages from 'vite-plugin-pages';
+import generateSitemap from 'vite-plugin-pages-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    Pages({
+      dirs: 'src/pages',
+      onRoutesGenerated: (routes) => generateSitemap({ routes }),
+    }),
+  ],
 });
