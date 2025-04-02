01775d79d2fcb0c04dffceb4d831d950b6bad97e
Subin Lee <ewaterbinn@gmail.com>
2025-04-02 17:38:43 +0900

    feat: vite-plugin-svgr 추가 및 관련 설정 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import Pages from 'vite-plugin-pages';
 import generateSitemap from 'vite-plugin-pages-sitemap';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    svgr(),
     Pages({
       dirs: 'src/pages',
       onRoutesGenerated: (routes) => generateSitemap({ routes }),
