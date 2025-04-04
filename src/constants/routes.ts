d98affdede446813335dac502cc0e5e92da6f072
Subin Lee <ewaterbinn@gmail.com>
2025-04-04 10:10:55 +0900

    refactor: 내/외부 링크 하나의 파일로 통합

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,3 +1,6 @@
+/**
+ * service links
+ */
 const ROUTES = {
   MAIN: `/`,
   POST: '/post',
@@ -6,4 +9,9 @@ const ROUTES = {
   ABOUT: `/about`,
 } as const;
 
-export { ROUTES };
+//external links
+const GITHUB_URL = 'https://github.com/delook-dev';
+const CONTRIBUTE_SERVICE = `${GITHUB_URL}/devlook/contribute`;
+const REPORT_BUGS = `${GITHUB_URL}/delook/issues`;
+
+export { CONTRIBUTE_SERVICE, GITHUB_URL, REPORT_BUGS, ROUTES };
