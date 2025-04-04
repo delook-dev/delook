d5917e494fe7bbc140481d8e767b289d4cd8ee2f
Subin Lee <ewaterbinn@gmail.com>
2025-04-04 10:07:38 +0900

    feat: mdx 렌더링 컴포넌트 추가, 랜덤 포스트 및 개별 포스트 로딩 기능 구현

---
+++ src/features/post/components/MDXHeader.tsx
@@ -0,0 +1,57 @@
+import { Bookmark, BookmarkCheck } from 'lucide-react';
+import { useState } from 'react';
+
+import { Badge, IconButton } from '@/components';
+import { PostMetaData } from '@/features/post/types/postTypes';
+
+export function MDXHeader({
+  metaData,
+  onBookmark,
+}: {
+  metaData: PostMetaData;
+  onBookmark: () => void;
+}) {
+  const [isBookmarked, setIsBookmarked] = useState<boolean>(false);
+
+  const handleBookmark = () => {
+    setIsBookmarked((prev) => !prev);
+    onBookmark();
+  };
+
+  return (
+    <header className="mt-3 flex flex-col items-start border-b border-b-muted pb-4">
+      <div className="flex w-full items-center justify-between">
+        <Badge
+          className="mb-2 rounded-lg bg-green px-1.5 text-background"
+          size={'xs'}
+          variant="green"
+        >
+          {metaData.language}
+        </Badge>
+        <IconButton buttonProps={{ className: 'hover:bg-transperant' }} onClick={handleBookmark}>
+          {isBookmarked ? <BookmarkCheck size={30} /> : <Bookmark size={30} />}
+        </IconButton>
+      </div>
+
+      <h1 className="mb-3 text-3xl font-medium">{metaData.title}</h1>
+
+      <span className="mb-2 text-[0.7rem] text-muted-foreground">
+        Updated On : {metaData.dateModified}
+      </span>
+
+      <div className="flex w-full flex-wrap items-center gap-1">
+        {metaData.tags.map((tag: string) => (
+          <Badge
+            className="whitespace-nowrap text-primary/60 dark:text-violet-400"
+            key={tag}
+            rounded="full"
+            size="xxs"
+            variant={'outline'}
+          >
+            {tag}
+          </Badge>
+        ))}
+      </div>
+    </header>
+  );
+}
