0a30017f731925cdd29a25a7439786034b227592
Subin Lee <ewaterbinn@gmail.com>
2025-04-12 17:12:20 +0900

    feat: 사이드바가 들어가는 페이지에 북마크 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,15 +3,17 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './footer/Footer';
 import Header from './header/Header';
 
+const sidebarPages = ['archive', 'bookmark'];
+
 export function Layout() {
   const pathname = useLocation().pathname;
-  const isArchivePage = pathname.includes('archive');
+  const isPageIncludeSidebar = sidebarPages.some((page) => pathname.includes(page));
 
   return (
     <div className="relative flex min-h-svh flex-col border-border bg-background lg:mx-4 xl:border-x xl:border-dashed">
       <Header />
       <main
-        className={`flex w-full flex-1 flex-col p-4 sm:mx-auto ${!isArchivePage ? 'max-w-3xl ' : 'md:p-0'}`}
+        className={`flex w-full flex-1 flex-col p-4 sm:mx-auto ${!isPageIncludeSidebar ? 'max-w-3xl ' : 'md:p-0'}`}
       >
         <Outlet />
       </main>
