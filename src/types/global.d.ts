706e6795b9e3ff5a50ec8844face003b1510646b
Subin Lee <ewaterbinn@gmail.com>
2025-04-12 19:39:55 +0900

    fix: 필터 value타입을 불린으로 수정

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,11 +1,11 @@
 import { STORAGE_KEYS } from '@/constants';
-import { CategorizedBookmarks } from '@/features/bookmark/types/bookmarkTypes';
+import { CategorizedBookmarks } from '@/features/bookmark';
 
 declare global {
   type StorageKeyValue = (typeof STORAGE_KEYS)[keyof typeof STORAGE_KEYS];
   type StorageKeyName = keyof typeof STORAGE_KEYS;
 
-  type FilteredCategory = Record<string, string>;
+  type FilteredCategory = Record<string, boolean>;
 
   type StorageValueMap = {
     saved_posts: CategorizedBookmarks;
