e3d457dcc8333a4723d10414f9391df0cb68c720
Subin Lee <ewaterbinn@gmail.com>
2025-04-12 17:23:38 +0900

    feat: 북마크 페이지 구현

--- src/pages/bookmark/index.tsx
+++ src/pages/bookmark/index.tsx
@@ -1,3 +1,30 @@
+import { useCallback } from 'react';
+
+import { BookmarkData, EmptyPage, getStorageList } from '@/features/bookmark';
+import { ErrorPage } from '@/features/error';
+import { PostSidebarLayout, RenderPost, usePostList } from '@/features/post';
+
 export default function BookmarkPage() {
-  return <div>bookmark</div>;
+  const fetchPostList = useCallback(async () => {
+    const { saved: posts } = await getStorageList();
+    return posts;
+  }, []);
+
+  const { post, categoryList, selectedPost } = usePostList<BookmarkData>({
+    fetchPostList,
+  });
+
+  if (categoryList.length === 0) {
+    return <EmptyPage />;
+  }
+
+  if (!post || !selectedPost) {
+    return <ErrorPage type="NOT_FOUND" />;
+  }
+
+  return (
+    <PostSidebarLayout categoryList={categoryList}>
+      <RenderPost post={{ ...post, ...selectedPost }} />
+    </PostSidebarLayout>
+  );
 }
