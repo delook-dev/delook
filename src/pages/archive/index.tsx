c3c2a5d26d6f62233844606ad61a9c863eb12d95
Subin Lee <ewaterbinn@gmail.com>
2025-04-12 17:19:12 +0900

    refactor: 분리한 컴포넌트 사용하여 코드 간소화

--- src/pages/archive/index.tsx
+++ src/pages/archive/index.tsx
@@ -1,44 +1,23 @@
-import { useMemo } from 'react';
+import { useCallback } from 'react';
 
-import { ArchiveLayout, CategoryList, useArchive } from '@/features/archive';
 import { ErrorPage } from '@/features/error';
-import { MDXHeader, MDXRender } from '@/features/post';
+import { getPostsByCategory, PostSidebarLayout, RenderPost, usePostList } from '@/features/post';
 
 export default function ArchivePage() {
-  const { postsByCategory, post, selectedPost } = useArchive();
+  const fetchPostList = useCallback(async () => {
+    const posts = await getPostsByCategory();
+    return posts;
+  }, []);
 
-  const categoryList: CategoryList[] = useMemo(() => {
-    return Object.entries(postsByCategory).map(([category, posts]) => ({
-      category,
-      posts: posts
-        .filter((post) => post.filename !== undefined)
-        .map((post) => ({
-          title: post.metaData.title,
-          filename: post.filename as string,
-        })),
-    }));
-  }, [postsByCategory]);
+  const { categoryList, post, selectedPost } = usePostList({ fetchPostList });
 
   if (!post || !selectedPost) {
     return <ErrorPage type="NOT_FOUND" />;
   }
 
-  const { metaData, Content } = post;
-  const { category, filename } = selectedPost;
-
   return (
-    <ArchiveLayout categoryList={categoryList}>
-      <div className="mx-auto max-w-3xl md:pl-6 md:pt-4">
-        <MDXHeader
-          isBookmarked={post.isBookmarked}
-          metaData={metaData}
-          pathData={{ category, filename }}
-        />
-
-        <MDXRender>
-          <Content />
-        </MDXRender>
-      </div>
-    </ArchiveLayout>
+    <PostSidebarLayout categoryList={categoryList}>
+      <RenderPost post={{ ...post, ...selectedPost }} />
+    </PostSidebarLayout>
   );
 }
