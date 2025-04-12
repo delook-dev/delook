c5df06957bdf7a0b4580c65110696dfe85904c14
Subin Lee <ewaterbinn@gmail.com>
2025-04-12 17:17:30 +0900

    refactor: collapsible 사이드바와 children을 감싸는 레이아웃 컴포넌트 분리

---
+++ src/features/post/components/PostSidebarLayout.tsx
@@ -0,0 +1,42 @@
+import { useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { SidebarCollapsibleMenu, SidebarLayout } from '@/components';
+
+import { CategoryList } from '../types/postTypes';
+
+export const PostSidebarLayout = ({
+  categoryList,
+  children,
+}: {
+  categoryList: CategoryList[];
+  children: React.ReactNode;
+}) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSearchParams = useCallback(
+    (category: string, filename: string) => {
+      setSearchParams({ category, filename });
+    },
+    [setSearchParams],
+  );
+
+  const sidebarMenu = categoryList.map(({ category, posts }) => (
+    <SidebarCollapsibleMenu
+      key={category}
+      label={category}
+      items={posts.map(({ filename, title }) => ({
+        key: filename,
+        title: title,
+        isActive: searchParams.get('filename') === filename,
+        onClick: () => handleSearchParams(category, filename),
+      }))}
+    />
+  ));
+
+  return (
+    <SidebarLayout sidebarMenu={sidebarMenu}>
+      <div className="mx-auto max-w-3xl md:pl-6 md:pt-4">{children}</div>
+    </SidebarLayout>
+  );
+};
