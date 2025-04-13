4453134fc7c1da98241f602876b88a4435dbcdbb
Subin Lee <ewaterbinn@gmail.com>
2025-04-13 15:31:36 +0900

    feat: popover trigger 인 경우 대응

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -7,13 +7,19 @@ export const IconButton = ({
   onClick,
   tooltipContent,
   buttonProps,
+  popover = false,
 }: {
   children: ReactNode;
   onClick?: () => void;
   tooltipContent?: string;
   buttonProps?: ButtonProps;
+  popover?: boolean;
 }) => {
-  const buttonElement = (
+  const buttonElement = popover ? (
+    <div className="flex size-9 items-center justify-center rounded-md hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
+      {children}
+    </div>
+  ) : (
     <Button size={'icon'} variant={'ghost'} onClick={onClick} {...buttonProps}>
       {children}
     </Button>
