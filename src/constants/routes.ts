031ffa48768a39e0395bcf7d18543a62bf9a9de5
Subin <ewaterbinn@gmail.com>
2025-04-17 00:03:22 +0900

    chore: contributing url 업데이트

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -13,7 +13,7 @@ const ROUTES = {
 
 //external links
 const GITHUB_URL = 'https://github.com/delook-dev';
-const CONTRIBUTE_SERVICE = `${GITHUB_URL}/delook/CONTRIBUTING.md`;
+const CONTRIBUTE_SERVICE = `${GITHUB_URL}/delook/blob/main/CONTRIBUTING.md`;
 const REPORT_BUGS = `${GITHUB_URL}/delook/issues`;
 
 export { CONTRIBUTE_SERVICE, GITHUB_URL, REPORT_BUGS, ROUTES, SITE_URL };
