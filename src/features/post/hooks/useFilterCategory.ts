acfd393fd79170035486705d9bd6745f7a35b3c2
Subin Lee <ewaterbinn@gmail.com>
2025-06-18 16:21:58 +0900

    fix: 필터 아이콘 클릭시 콘텐츠 바뀌는 오류 수정, 저장만 눌러도 포스트 바뀌는 오류 수정 #65

--- src/features/post/hooks/useFilterCategory.ts
+++ src/features/post/hooks/useFilterCategory.ts
@@ -1,7 +1,9 @@
 import { useCallback, useEffect, useState } from 'react';
 
+import { ToastMsg } from '@/constants';
 import { getPostsByCategory } from '@/features/post';
 import { useFilterStore } from '@/features/post/store/useFilterStore';
+import { toast } from '@/hooks';
 
 type CategoryItem = { id: string; label: string };
 type SettingsMap = Record<string, boolean>;
@@ -31,13 +33,28 @@ export function useFilterCategory() {
     fetchCategories();
   }, [fetchCategories]);
 
-  const saveSettings = async (nextChecked: string[]) => {
-    const next: SettingsMap = Object.fromEntries(
-      categories.map((c) => [c.id, nextChecked.includes(c.id)]),
-    );
-    await setFilter(next);
-    setChecked(nextChecked);
-  };
+  const saveSettings = useCallback(
+    async (selectedCategories: string[]) => {
+      // 이전과 동일하면 저장하지 않음
+      if (JSON.stringify(selectedCategories) === JSON.stringify(checked)) {
+        toast({
+          title: ToastMsg.filterError,
+        });
+        return;
+      }
+
+      const newFilterSettings: SettingsMap = Object.fromEntries(
+        categories.map((c) => [c.id, selectedCategories.includes(c.id)]),
+      );
+
+      await setFilter(newFilterSettings);
+      setChecked(selectedCategories);
+      toast({
+        title: ToastMsg.filterSuccess,
+      });
+    },
+    [categories, checked, setFilter],
+  );
 
   return {
     categories,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { RenderPost, useFilterStore, usePostStore } from '@/features/post';
 
 function PostContent() {
   const { filter } = useFilterStore();
+
   const { currentPost, randomPost, isError } = usePostStore(
     useShallow((state) => ({
       currentPost: state.currentPost,
@@ -17,7 +18,14 @@ function PostContent() {
 
   useEffect(() => {
     randomPost();
-  }, [filter, randomPost]);
+  }, [randomPost]);
+
+  useEffect(() => {
+    //현재 포스트의 카테고리가 선택한 카테고리에 포함되어 있지 않으면 랜덤 포스트 재생성
+    if (filter && currentPost && !filter[currentPost.category]) {
+      randomPost();
+    }
+  }, [filter, randomPost, currentPost]);
 
   if (!currentPost) return null;
   if (isError) return <ErrorPage type="NOT_FOUND" />;
