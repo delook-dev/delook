90c84000da904dd72001347f6e55d7ea08e141ad
waterbinnn <ewaterbin@naver.com>
2025-08-19 01:05:13 +0900

    feat: 댑댑댑 스타일 개선, 말풍선 툴팁 추가

--- src/components/ui/DevDevDev.tsx
+++ src/components/ui/DevDevDev.tsx
@@ -1,11 +1,28 @@
 import { ExternalLink } from './ExternalLink';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './Tooltip';
 
-const DEVDEVDEV_URL = 'https://www.devdevdev.co.kr/main';
+const DEVDEVDEV_URL = 'https://www.devdevdev.co.kr/techblog';
 
 export const DevDevDev = () => {
   return (
-    <ExternalLink href={DEVDEVDEV_URL} className="fixed bottom-12 right-4 w-32">
-      <img className="" src="/images/devdevdev.png" alt="댑댑댑" />
-    </ExternalLink>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <ExternalLink href={DEVDEVDEV_URL} className="fixed bottom-14 right-4 w-32">
+            <img className="drop-shadow-md" src="/images/devdevdev.png" alt="댑댑댑" />
+          </ExternalLink>
+        </TooltipTrigger>
+        <TooltipContent className="overflow-visible bg-yellow text-center text-xs  text-stone-600 drop-shadow-md">
+          <div className="relative">
+            <span className="whitespace-pre">{`국내 빅테크 기업들의\n블로그 포스팅 보러가기`}</span>
+            <span
+              className="absolute left-1/2 top-full size-0 -translate-x-1/2
+          translate-y-1 border-x-8
+          border-t-8 border-x-transparent border-t-yellow"
+            />
+          </div>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
